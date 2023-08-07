cb31a327d9bee1345c354db8b6bcbbdaf3c073ee
badahae <kdh6937@naver.com>
2023-08-07 12:43:35 +0900

    Key

--- src/hi/Hi_java.java
+++ src/hi/Hi_java.java
@@ -6,5 +6,6 @@ public class Hi_java {
 		System.out.println("Hello");
 		System.out.println("성공했네 ~~~~ 기쁘다 !!!");
 		System.out.println("없어지는지 확인용");
+		System.out.println("key_업로드 안 되게 폴더 밖으로 빼둠");
 	}
 }
